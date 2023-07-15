58436305b7f462076e6948db10f021cb407ab6a2
tbackens <99706186+tbackens@users.noreply.github.com>
2023-07-15 05:42:05 +0200

    added dashboard component

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { useSignOut } from 'react-auth-kit'
+
 
 const Dashboard = () => {
+  const signout = useSignOut()
+
   return (
-    <div>Dashboard</div>
+    <div className='d-flex flex-column shadow w-50 p-3 mb-5 rounded text-white align-items-center justify-content-center m-auto'>
+      <h1 className='text-white'>Hi testuser</h1>
+      <p>Welcome to your Dashboard</p>
+      <button className='btn btn-primary' onClick={()=> signout()}>Logout</button>
+    </div>
   )
 }
 
