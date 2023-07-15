50d4f82bd0649e7cbc5005e6bb47dd366ec087fc
tbackens <99706186+tbackens@users.noreply.github.com>
2023-07-15 05:44:23 +0200

    added form

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,23 +2,23 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div className='d-flex flex-column shadow w-25 p-3 mb-5 rounded text-white align-items-center justify-content-center'>
+    <div className='d-flex flex-column shadow p-3 w-50 rounded text-white align-items-center justify-content-center m-auto'>
         <form>
-            <h3 className=''>Login</h3>
-            <div class="mb-3">
-                <label for="email" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="email" aria-describedby="emailHelp"/>
+            <h3 className=''>Register</h3>
+            <div className="mb-3">
+                <label htmlFor="email" className="form-label">Email address</label>
+                <input type="email" className="form-control" id="email" aria-describedby="emailHelp"/>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="password1" className="form-label">Password</label>
+                <input type="password" className="form-control" id="password1"/>
             </div>
             <div class="mb-3">
-                <label for="password1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="password1"/>
+                <label htmlFor="password2" className="form-label">Repeat Password</label>
+                <input type="password" className="form-control" id="password2"/>
             </div>
-            <div class="mb-3">
-                <label for="password2" class="form-label">Repeat Password</label>
-                <input type="password" class="form-control" id="password2"/>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
         </form>
+        <button type="submit" className="btn btn-primary">Submit</button>
     </div>
   )
 }
